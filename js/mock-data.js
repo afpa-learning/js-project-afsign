955021e0085efe60b449f98153c8cd361905e62a
Ludovic Esperce <ludovic.esperce@afpa.fr>
2023-10-30 09:06:10 +0100

    Initial commit

---
+++ js/mock-data.js
@@ -0,0 +1,147 @@
+/**
+ * JSon contenant les informations d'une session de formation ainsi que les stagiaires associés.
+ */
+let trainingSession = {
+    "id": 1,
+    "nom": "DWWM",
+    "date": "10/30/2023",
+    "start_hour": 13,
+    "end_hour": 17,
+    "stagiaires": [
+        {
+            "id": 1,
+            "first_name": "Conrado",
+            "last_name": "Myers",
+            "date_inscription": "10/18/2021",
+            "email": "cmyers0@ca.gov",
+            "status": "ABSENT", // le status peut prendre 3 valeurs : "NOT_SPECIFIED", "PRESENT", "ABSENT"
+            "has_signed": true
+        },
+        {
+            "id": 2,
+            "first_name": "Rockey",
+            "last_name": "Neeve",
+            "date_inscription": "09/13/2021",
+            "email": "rneeve1@va.gov",
+            "status": "NOT_SPECIFIED",
+            "has_signed": true
+        },
+        {
+            "id": 3,
+            "first_name": "Lee",
+            "last_name": "Tookill",
+            "date_inscription": "05/23/2023",
+            "email": "ltookill2@irs.gov",
+            "status": "NOT_SPECIFIED",
+            "has_signed": true
+        },
+        {
+            "id": 4,
+            "first_name": "Tania",
+            "last_name": "Pamphilon",
+            "date_inscription": "08/12/2021",
+            "email": "tpamphilon3@istockphoto.com",
+            "status": "NOT_SPECIFIED",
+            "has_signed": true,
+        },
+        {
+            "id": 5,
+            "first_name": "Ingeborg",
+            "last_name": "Collingridge",
+            "date_inscription": "09/16/2021",
+            "email": "icollingridge4@webmd.com",
+            "status": "NOT_SPECIFIED",
+            "has_signed": true
+        },
+        {
+            "id": 6,
+            "first_name": "Artur",
+            "last_name": "Han",
+            "date_inscription": "02/22/2022",
+            "email": "ahan5@hhs.gov",
+            "status": "ABSENT",
+            "has_signed": false
+        },
+        {
+            "id": 7,
+            "first_name": "Nady",
+            "last_name": "Kearley",
+            "date_inscription": "10/25/2023",
+            "email": "nkearley6@noaa.gov",
+            "status": "NOT_SPECIFIED",
+            "has_signed": false
+        },
+        {
+            "id": 8,
+            "first_name": "Casey",
+            "last_name": "Rodenburg",
+            "date_inscription": "02/14/2022",
+            "email": "crodenburg7@cocolog-nifty.com",
+            "status": "NOT_SPECIFIED",
+            "has_signed": false
+        },
+        {
+            "id": 9,
+            "first_name": "Ring",
+            "last_name": "Knighton",
+            "date_inscription": "04/02/2023",
+            "email": "rknighton8@artisteer.com",
+            "status": "NOT_SPECIFIED",
+            "has_signed": false
+        },
+        {
+            "id": 10,
+            "first_name": "Jessamine",
+            "last_name": "Brisse",
+            "date_inscription": "04/14/2022",
+            "email": "jbrisse9@comcast.net",
+            "status": "NOT_SPECIFIED",
+            "has_signed": false
+        },
+        {
+            "id": 11,
+            "first_name": "Todd",
+            "last_name": "Bateman",
+            "date_inscription": "11/08/2021",
+            "email": "tbatemana@lycos.com",
+            "status": "PRESENT",
+            "has_signed": true
+        },
+        {
+            "id": 12,
+            "first_name": "Milli",
+            "last_name": "Kingsford",
+            "date_inscription": "04/05/2022",
+            "email": "mkingsfordb@wordpress.org",
+            "status": "PRESENT",
+            "has_signed": false
+        },
+        {
+            "id": 13,
+            "first_name": "George",
+            "last_name": "Vynarde",
+            "date_inscription": "01/23/2023",
+            "email": "gvynardec@china.com.cn",
+            "status": "ABSENT",
+            "has_signed": false
+        },
+        {
+            "id": 14,
+            "first_name": "Kelci",
+            "last_name": "Rickerd",
+            "date_inscription": "05/29/2023",
+            "email": "krickerdd@last.fm",
+            "status": "NOT_SPECIFIED",
+            "has_signed": false
+        },
+        {
+            "id": 15,
+            "first_name": "Saxe",
+            "last_name": "Larn",
+            "date_inscription": "10/12/2022",
+            "email": "slarne@360.cn",
+            "status": "ABSENT",
+            "has_signed": false
+        }]
+
+};
---
+++ js/script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // récupération de la session à partir du fichier "mock-data.js"
+    let session = trainingSession;
+    // récupération de tous les stagiaires
+    let stagiaires = session.stagiaires;
+
+    // boucle sur tous les stagaires
+    for (let i = 0; i < stagiaires.length; i++) {
+        console.log(stagiaires[i]);
+
+        // ajouter ICI le code pour ajouter les div à la page
+    }
+});
+
+/**
+ * Compte les stagiaires absents.
+ * 
+ * @param {*} stagiaires un tableau de stagiaires
+ * @returns le nombre de stagiaires absents
+ */
+function countAbsents(stagiaires) {
+    // logique de comptage à ajouter ici
+
+    return -1;
+}
+
+/**
+ * Comptes
+ * @param {*} stagiaires
+ * @returns le nombre de stagiaires au status "NOT_SPECIFIED"
+ */
+function countNotSpecifiedStatus(stagiaires) {
+
+}
+
+/**
+ * Retourne 
+ * @param {*} stagaires 
+ */
+function findByFirstAndLastName(stagaires) {
+
+}
+
+// Exemple de code de création de div (à adatper) :
+// // création de la "div"
+// let nouvelleDiv = document.createElement('div');
+// // ajout d'un id
+// div.id = 'mettre ici un id';
+// // ajout du contenu html de la div
+// div.innerHTML = 'mettre ici le contenu html de la div. Tout est possible !';
+// // ajout d'une ou plusieurs classes
+// div.className = 'nom-classe-1 nom-classe-2 ...';
+
+// // ajout de la div avec la méthode "appendChild" (pas forcément sur le "body" mais là où vous le souhaitez)
+// document.body.appendChild(nouvelleDiv);
